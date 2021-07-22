538acd9f7a8f99d311d481de949e5f1889176dba
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2021-07-22 20:12:51 +0530

    bug fix : type object user has no attribute USERNAME_FIELD

--- app/core/models.py
+++ app/core/models.py
@@ -13,11 +13,11 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser,PermissionsMixin):
     """Custom user model that supports email instead of username"""
-    email = models.EmailField(max_length=256)
+    email = models.EmailField(max_length=256,unique=True)
     name = models.CharField(max_length=256)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
-    USERNAME_FIELD = email
+    USERNAME_FIELD = 'email'
 
